0b15fde659eaf6e2fd577358da20e28688cc0319
Camila Rojas <90579964+camilarojas11@users.noreply.github.com>
2022-03-31 00:03:38 -0300

    Agrego carpeta de templates

--- appcoder/views.py
+++ appcoder/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
 
 # Create your views here.
+logo = "App Coder"
+items = ["Home", "Usuarios", "Servicios", "Pagos"]
+
+def index(request):
+    id = 1
+    h1_index = "Hola! Bienveni@ a la APP Coder"
+    subtitulo_index = "Esta entrega corresponde a la primer entrega del proyecto final."
+    dict_context = {'id': id, 'h1_index': h1_index, 'items': items, 'subtitulo_index':subtitulo_index, 'logo': logo}
+    return render(request, 'appcoder/index.html',dict_context)
+

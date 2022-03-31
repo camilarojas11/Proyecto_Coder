b139ea2e43f3f9eee8420ffdd36c88f9b9da2a62
Camila Rojas <90579964+camilarojas11@users.noreply.github.com>
2022-03-31 00:27:19 -0300

    Agrego los modelos de la app

--- proyecto_coder/proyecto_coder/settings.py
+++ proyecto_coder/proyecto_coder/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appcoder',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'proyecto_coder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['C:/Users/camil/OneDrive/Escritorio/Python/Entrega_Intermedia/proyecto_coder/appcoder/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
